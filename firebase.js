5e78c98a940502c44dfdb5a80710eee2417c7367
Junior Rodrigues <123584888+Rodr77gues@users.noreply.github.com>
2025-11-26 13:10:12 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyCEtq-uC5EfUAIseO7dEPL5QEfoZLYaSgU",
+  authDomain: "nextstop-a15ad.firebaseapp.com",
+  projectId: "nextstop-a15ad",
+  storageBucket: "nextstop-a15ad.firebasestorage.app",
+  messagingSenderId: "340141470022",
+  appId: "1:340141470022:web:8de279a702cc25827f7547"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
